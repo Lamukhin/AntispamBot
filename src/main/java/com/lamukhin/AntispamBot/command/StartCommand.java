87105745310290435564dc71d7ffe321e9fc643f
Yuriy <lamukhin.yuriy@gmail.com>
2024-05-31 18:17:38 +0300

    возможно это тоже вариант

--- src/main/java/com/lamukhin/AntispamBot/command/StartCommand.java
+++ src/main/java/com/lamukhin/AntispamBot/command/StartCommand.java
@@ -4,18 +4,10 @@ import com.lamukhin.AntispamBot.util.MessageOperations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.wdeath.managerbot.lib.bot.TelegramLongPollingEngine;
 import ru.wdeath.managerbot.lib.bot.annotations.CommandFirst;
 import ru.wdeath.managerbot.lib.bot.annotations.CommandNames;
-import ru.wdeath.managerbot.lib.bot.annotations.CommandOther;
 import ru.wdeath.managerbot.lib.bot.annotations.ParamName;
-import ru.wdeath.managerbot.lib.bot.command.CommandContext;
-import ru.wdeath.managerbot.lib.bot.session.UserBotSession;
-
-import java.sql.Timestamp;
 
 import static com.lamukhin.AntispamBot.util.ResponseMessage.*;
 
@@ -31,29 +23,47 @@ public class StartCommand {
     @CommandFirst
     public void greeting(TelegramLongPollingEngine engine,
                          @ParamName("chatId") Long chatId) {
-        if (System.currentTimeMillis() - helloTime < 1800000) {
-            if (justStarted) {
-                MessageOperations.sendNewMessage(
-                        chatId,
-                        HELLO,
-                        engine);
-                helloTime = System.currentTimeMillis();
-                justStarted = false;
-            } else if (!floodCatched) {
-                MessageOperations.sendNewMessage(
-                        chatId,
-                        DONT_FLOOD,
-                        engine);
-                floodCatched = true;
-            }
-        } else {
+        if (System.currentTimeMillis() - helloTime >= 1800000) {
+            floodCatched = true;
+        }
+
+        if (justStarted) {
             MessageOperations.sendNewMessage(
                     chatId,
                     HELLO,
                     engine);
             helloTime = System.currentTimeMillis();
+            justStarted = false;
+        } else if (!floodCatched) {
+            MessageOperations.sendNewMessage(
+                    chatId,
+                    DONT_FLOOD,
+                    engine);
             floodCatched = true;
         }
+//        if (System.currentTimeMillis() - helloTime < 1800000) {
+//            if (justStarted) {
+//                MessageOperations.sendNewMessage(
+//                        chatId,
+//                        HELLO,
+//                        engine);
+//                helloTime = System.currentTimeMillis();
+//                justStarted = false;
+//            } else if (!floodCatched) {
+//                MessageOperations.sendNewMessage(
+//                        chatId,
+//                        DONT_FLOOD,
+//                        engine);
+//                floodCatched = true;
+//            }
+//        } else {
+//            MessageOperations.sendNewMessage(
+//                    chatId,
+//                    HELLO,
+//                    engine);
+//            helloTime = System.currentTimeMillis();
+//            floodCatched = true;
+//        }
 
     }
 
